0f3f94008dac578321565797e65ea9768b4936da
Maitreyi Thakkar <mthakka2@asu.edu>
2018-08-10 00:52:32 -0700

    adding front end files

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ class App extends Component {
     this.setState({imageUrl : this.state.input});
     // app.models.predict(
     //   Clarifai.FACE_DETECT_MODEL, this.state.input)
-    fetch('http://localhost:3000/imageurl' ,{
+    fetch('https://secure-atoll-51459.herokuapp.com/imageurl' ,{
           method: 'post',
           headers: {'Content-Type' : 'application/json'},
           body: JSON.stringify({
@@ -120,7 +120,7 @@ class App extends Component {
     .then(response => response.json())
     .then (response => {
       if(response){
-        fetch('http://localhost:3000/image' ,{
+        fetch('https://secure-atoll-51459.herokuapp.com/image' ,{
           method: 'put',
           headers: {'Content-Type' : 'application/json'},
           body: JSON.stringify({
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component {
 
   onSubmitSignIn = () => {
     //event.preventDefault();
-    fetch('http://localhost:3000/register', {
+    fetch('https://secure-atoll-51459.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type' : 'application/json'},
       body: JSON.stringify({
